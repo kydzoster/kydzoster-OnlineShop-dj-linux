446db6192074a189c3633023319397c0b667fc2f
Martins <kydzoster@gmail.com>
2020-07-28 09:50:37 +0100

    registered catalog models on the admin site

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    # value is automatically set using the value from other fields (name), this is for generating slug
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    # this will allow to edit multiple rows from the list_display field
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
