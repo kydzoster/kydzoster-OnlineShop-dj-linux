6a1bddf3cf480f7409ad6562e3c560663b00cbd4
Martins <kydzoster@gmail.com>
2020-07-28 14:35:30 +0100

    added order models, registers customer oreders

--- myshop/settings.py
+++ myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # custom
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
---
+++ orders/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from shop.models import Product
+
+# stores customer information and a paid boolean field which defaults to False, 
+# its purpose will be to diferentiate paid and unpaid orders.
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'Order {self.id}'
+    # obtains total cost of items bought in this order
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+# allows to store product, quantity and price paid for each item.
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    # returns the cost of item.
+    def get_cost(self):
+        return self.price * self.quantity
