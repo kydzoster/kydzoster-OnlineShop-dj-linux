1ff9bc283ec930a6cf5455ab34ec4392b1796d18
Martins <kydzoster@gmail.com>
2020-07-28 10:51:07 +0100

    added products to the cart in shop views

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 # this view will retrieve and list all the products by a given category
 def product_list(request, category_slug=None):
@@ -20,6 +21,8 @@ def product_list(request, category_slug=None):
 # this view will retrieve and display a single product
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {
         'product': product,
+        'cart_product_form': cart_product_form
         })
